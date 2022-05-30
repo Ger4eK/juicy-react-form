22ed660873b08f14788926127f75948907ebd3e4
Ger4eK <ger4ek228@gmail.com>
2022-05-31 00:13:22 +0300

    added Header

--- src/App.js
+++ src/App.js
@@ -1,17 +1,17 @@
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import { Header } from './Header';
 
 const Step1 = () => <>Step 1</>;
 const Step2 = () => <>Step 2</>;
 const Step3 = () => <>Step 3</>;
 const Result = () => <>Result</>;
 
-const Header = () => <h1>Потна формОчка</h1>;
-
 function App() {
   return (
     <>
       <Header />
       <BrowserRouter>
+      
         <Routes>
           <Route path='/' element={<Step1 />} />
           <Route path='/step2' element={<Step2 />} />
---
+++ src/Header.js
@@ -0,0 +1,22 @@
+import { makeStyles, Typography } from '@material-ui/core';
+
+const useStyles = makeStyles((theme) => ({
+  root: {
+    fontFamily: 'Permanent Marker',
+    margin: theme.spacing(3, 0, 2),
+    textAlign: 'center',
+    fontSize: '40px',
+    color: 'deeppink',
+    textShadow: '1px 1px darkmagenta',
+  },
+}));
+
+export const Header = () => {
+  const styles = useStyles();
+
+  return (
+    <Typography className={styles.root} component='h1' variant='h5'>
+      Juicy Form
+    </Typography>
+  );
+};
